3f0b2056ffaa4f0a71c07e3fa3b278bdac90d88a
JUPJEET1902U <singhjupjeet42@gmail.com>
2026-06-22 17:18:49 +0530

    Update fun.js

--- commands/fun.js
+++ commands/fun.js
@@ -1,4 +1,3 @@
-const { AttachmentBuilder } = require("discord.js")
 const { callAI } = require("../utils/ai")
 const { addRoast, getLeaderboard } = require("../utils/roast")
 const { checkCooldown } = require("../utils/cooldowns")
