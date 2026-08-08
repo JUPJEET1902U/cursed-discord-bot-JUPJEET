f6308d9ca9c215811d258a331570f8c06de1f8bf
JUPJEET1902U <singhjupjeet42@gmail.com>
2026-08-08 21:11:44 +0530

    reboot(roles): handle persistent reaction-role buttons

--- commands/powerRuntime.js
+++ commands/powerRuntime.js
@@ -3,6 +3,7 @@ const {
     startGiveawayScheduler,
     handleGiveawayButton,
 } = require("../utils/giveawayService")
+const { handleReactionRoleButton } = require("../utils/reactionRoleService")
 
 async function handle(message) {
     if (message?.client) startGiveawayScheduler(message.client)
@@ -14,6 +15,7 @@ if (!moderation.__powerRuntimePatched) {
     moderation.handleInteraction = async function patchedPowerRuntimeInteraction(interaction) {
         if (interaction?.client) startGiveawayScheduler(interaction.client)
         if (await handleGiveawayButton(interaction)) return true
+        if (await handleReactionRoleButton(interaction)) return true
         return originalHandleInteraction(interaction)
     }
     Object.defineProperty(moderation, "__powerRuntimePatched", { value: true, enumerable: false })
